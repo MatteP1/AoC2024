fc0820d276331fa4a84a5eafb33efbd4171aafad
Mathias Pedersen <mathiasp9999@gmail.com>
2024-12-05 22:20:18 +0100

    initial code for day 4

---
+++ day4/app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+-- Strategy. 
+-- Store input as a map with keys being pairs (x,y) and values being letters. 
+-- Create 8? functions to check each of the 8 directions from a point (x,y) for XMAS
+-- If a check passes: +1
+-- Do this for each point containing an 'X'
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
