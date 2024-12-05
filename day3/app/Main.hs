91e7ab02eac637bb1cf7373981ee907494809159
Mathias Pedersen <mathiasp9999@gmail.com>
2024-12-05 21:51:49 +0100

    finished part 2 of day 3.

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -10,6 +10,7 @@ type Prog = [MulInstr]
 
 type Parser = Parsec Void String
 
+-- part 1
 parseNum :: Parser Int
 parseNum = do
   num <- count' 1 3 digitChar
@@ -28,8 +29,28 @@ parseMulInstr = do
   _ <- char ')'
   return (n1, n2)
 
-parseFile :: Parser Prog
-parseFile = many (try $ skipManyTill anySingle (try parseMulInstr))
+parseProg :: Parser Prog
+parseProg = many (try $ skipManyTill anySingle (try parseMulInstr))
+
+-- part 2
+parseDo :: Parser String
+parseDo = string "do()"
+
+parseDont :: Parser String
+parseDont = string "don't()"
+
+parseNextDo :: Parser String
+parseNextDo = skipManyTill anySingle parseDo
+
+parseNextDont :: Parser String
+parseNextDont = manyTill anySingle parseDont
+
+parsePreprocess :: Parser String
+parsePreprocess =
+  do
+    filteredInput <- many (try $ parseNextDont <* parseNextDo)
+    rest <- parseNextDont <* takeRest
+    return $ concat filteredInput ++ rest
 
 parseFromFile :: Parsec e String a -> String -> IO (Either (ParseErrorBundle String e) a)
 parseFromFile p file = runParser p file <$> readFile file
@@ -38,8 +59,20 @@ exec :: Prog -> Int
 exec instrs = sum $ map (\(a, b) -> a * b) instrs
 
 main :: IO ()
-main = do
-  parsed <- parseFromFile parseFile "input.txt"
-  case parsed of
-    Left bundle -> putStr (errorBundlePretty bundle)
-    Right xs -> print $ exec xs
+main =
+  do
+    -- part 1
+    parsed1 <- parseFromFile parseProg "input.txt"
+    case parsed1 of
+      Left bundle -> putStr (errorBundlePretty bundle)
+      Right xs -> print $ exec xs
+    
+    -- part 2
+    parsed2 <- parseFromFile parsePreprocess "input.txt"
+    case parsed2 of
+      Left bundle -> putStr (errorBundlePretty bundle)
+      Right cleanedInput ->
+        let prog = runParser parseProg "" cleanedInput in
+        case prog of
+          Left bundle -> putStr (errorBundlePretty bundle)
+          Right xs -> print $ exec xs
