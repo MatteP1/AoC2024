5cb2e65dcebb930e76fa4120c6123113aaa46560
Mathias Pedersen <mathiasp9999@gmail.com>
2024-12-22 12:28:40 +0100

    finished part 2 of day 22

--- day22/app/Main.hs
+++ day22/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Data.Bits
+import Linear.V4
+import Data.Map as M (Map, alter, empty, elems)
+import Data.Set as S (Set, empty, insert, notMember)
+import Control.Monad.State ( State, MonadState (get), modify, execState)
+import Control.Monad (guard, when)
 
 mix :: Int -> Int -> Int
 mix x y = x .^. y
@@ -21,15 +26,43 @@ step3 x = prune $ mix (x .<<. 11) x
 nextSecretNumber :: Int -> Int
 nextSecretNumber = step3 . step2 . step1
 
-getSN2000 :: Int -> Int
-getSN2000 x = iterate nextSecretNumber x !! 2000
+getSecretNumbers :: Int -> [Int]
+getSecretNumbers x = iterate nextSecretNumber x
+
+
+type Sequence = V4 Int
+
+findSequenceValues :: [(Int, Int)] -> State (Map Sequence Int) ()
+findSequenceValues s =
+  let inner :: Set Sequence -> [(Int, Int)] -> State (Map Sequence Int) ()
+      inner done ((_, x1):(next@((_, x2):(_, x3):(sv, x4):_))) =
+        let currSequence = V4 x1 x2 x3 x4 in
+          do
+            when (currSequence `notMember` done) $ modify (alter (\svMaybe -> case svMaybe of {Just sv' -> Just (sv' + sv); Nothing -> Just sv}) currSequence)
+            inner (currSequence `insert` done) next
+      inner _ _ = return ()
+  in
+    inner S.empty s
+
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let initialSecrets = map read $ lines input :: [Int]
-      secretNumbers2000 = map getSN2000 initialSecrets
-    in
-    print $ sum secretNumbers2000
+      secretNumbers = map ((take 2001) . getSecretNumbers) initialSecrets
+
+      secretNumbers2000 = map last secretNumbers
+
+      allCosts = map (\secnums -> map (`mod` 10) secnums) secretNumbers
+
+      changes = map (\costs -> zipWith (-) (tail costs) costs) allCosts
+
+      allCostChanges = map (\(p1, p2) -> zip p1 p2) $ zip (map (drop 1) allCosts) changes
+
+      sequenceValues = execState (traverse findSequenceValues allCostChanges) M.empty
+
+    in do
+      print $ sum secretNumbers2000
+      print $ maximum (elems sequenceValues) -- takes ~10 seconds
 
 
